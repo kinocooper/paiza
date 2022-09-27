2684a6128fd278b55c0a9c3895c8900bab56beb4
kinocooper <95660939+kinocooper@users.noreply.github.com>
2022-09-27 09:16:59 +0900

    氷の城

---
+++ knight_and_which/氷の城.js
@@ -0,0 +1,20 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  if (lines[0] > 0) {
+    console.log(lines[0]);
+  }
+  else {
+    console.log("freeze");
+  }
+}); 
